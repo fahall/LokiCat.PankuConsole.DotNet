995e170e710f7ace24426c439a500139af18bac6
Alex Hall <alex@alexhall.org>
2025-05-23 14:25:47 -0500

    Add Attributes project

---
+++ src/LokiCat.PankuConsole.DotNet.Attributes/ConsoleCommandAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace LokiCat.PankuConsole.DotNet.Attributes;
+/// <summary>
+/// Marks a method to be exposed in PankuConsole.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class ConsoleCommandAttribute(string commandName = "") : Attribute
+{
+    public string CommandName { get; } = commandName;
+}
